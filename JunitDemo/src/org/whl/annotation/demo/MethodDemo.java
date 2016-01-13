d0496a41b4229edcf4077bb2e38dffae8d0c65ea
TomJarray <huangtudi_007@126.com>
2016-01-13 09:10:16 +0800

    Change folders layout

---
+++ JunitDemo/src/org/whl/annotation/demo/MethodDemo.java
@@ -0,0 +1,16 @@
+package org.whl.annotation.demo;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MethodDemo {
+
+    public String name() default "whl";
+
+}
